7d133769c871f64374d7af42ff1a7bc8c0aae4b3
gabrielxcaramelo <106408431+gabrielxcaramelo@users.noreply.github.com>
2023-10-02 21:12:40 -0300

    Add files via upload

---
+++ estoque/index.php
@@ -0,0 +1,135 @@
+<?php
+
+    if(isset($_POST['submit']))
+    {
+        
+
+        include_once('config.php');
+
+        $nome = $_POST['nome'];
+        $sobrenome = $_POST['sobrenome'];
+        $email = $_POST['email'];
+        $idade = $_POST['idade'];
+
+        $result = mysqli_query($conexao, "INSERT INTO cadastros(nome,sobrenome,email,idade) 
+        VALUES ('$nome','$sobrenome','$email','$idade')");
+
+      
+    }
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário | Exemplo</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            background-color:orange;
+        }
+        .box{
+            color: white;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            background-color: rgba(16, 7, 0, 5.6);
+            padding: 15px;
+            border-radius: 15px;
+            width: 20%;
+        }
+        fieldset{
+            border: 3px solid dodgerblue;
+        }
+        legend{
+            border: 1px solid dodgerblue;
+            padding: 10px;
+            text-align: center;
+            background-color: dodgerblue;
+            border-radius: 8px;
+        }
+        .inputBox{
+            position: relative;
+        }
+        .inputUser{
+            background: none;
+            border: none;
+            border-bottom: 1px solid white;
+            outline: none;
+            color: white;
+            font-size: 15px;
+            width: 100%;
+            letter-spacing: 2px;
+        }
+        .labelInput{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            pointer-events: none;
+            transition: .5s;
+        }
+        .inputUser:focus ~ .labelInput,
+        .inputUser:valid ~ .labelInput{
+            top: -20px;
+            font-size: 12px;
+            color: dodgerblue;
+        }
+        #data_nascimento{
+            border: none;
+            padding: 8px;
+            border-radius: 10px;
+            outline: none;
+            font-size: 15px;
+        }
+        #submit{
+            background-image: linear-gradient(to right,rgb(0, 92, 197), rgb(90, 20, 220));
+            width: 100%;
+            border: none;
+            padding: 15px;
+            color: white;
+            font-size: 15px;
+            cursor: pointer;
+            border-radius: 15px;
+        }
+        #submit:hover{
+            background-image: linear-gradient(to right,rgb(0, 80, 172), rgb(80, 19, 195));
+        }
+    </style>
+</head>
+<body>
+    <div class="box">
+        <form action="index.php" method="POST">
+            <fieldset>
+                <legend><b>Estoque</b></legend>
+                <br>
+                <div class="inputBox">
+                    <input type="text" name="nome" id="nome" class="inputUser" required>
+                    <label for="nome" class="labelInput">nome </label>
+                </div>
+                <br>
+                <div class="inputBox">
+                    <input type="sobrenome" name="sobrenome" id="sobrenome" class="inputUser" required>
+                    <label for="sobrenome" class="labelInput">sobrenome</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="email" id="email" class="inputUser" required>
+                    <label for="email" class="labelInput">email</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="idade" name="idade" id="idade" class="inputUser" required>
+                    <label for="idade" class="labelInput">idade</label>
+                </div>
+                <br><br>
+                <input type="submit" name="submit" id="submit">
+            </fieldset>
+               
+               
+</html>
